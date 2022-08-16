bb34e7869ba4f27552b6aec1b736e8d28c40e7dd
IbrahimSaffi <ibrahimsaffinew@gmail.com>
2022-08-16 23:05:31 +0500

    commit

--- src/components/CardsContainer.jsx
+++ src/components/CardsContainer.jsx
@@ -2,12 +2,6 @@ import React, { useEffect, useState } from 'react'
 import Card from './Card'
 
 export default function CardsContainer(props) {
-    // let [pokemonPage,setPokemonPage] = useState([])
-    // useEffect(()=>{
-    //     fetch(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=${(props.pageNo-1)*20}`)
-    //     .then(resp=>resp.json())
-    //     .then(data=>setPokemonPage(data.results))
-    // },[props.pageNo])
     console.log(props.pokemonPage)
   return (
     <div className='cards-container' >
